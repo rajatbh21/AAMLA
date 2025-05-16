cbd2504c944c3215921ca9bdb27f873cb443c7fa
Hyunwoo Oh <hyunwooo@uci.edu>
2025-05-16 11:37:00 -0700

    modify mezo for dependency

--- MeZO/large_models/mezo.sh
+++ MeZO/large_models/mezo.sh
@@ -74,7 +74,7 @@ echo "TRAIN/EVAL STEPS: $STEPS/$EVAL_STEPS"
 echo "MODE: $MODE"
 echo "Extra args: $EXTRA_ARGS $TASK_ARGS"
 
-CUDA_VISIBLE_DEVICES=$GPU python -m torch.distributed.run --nproc_per_node 1 run.py \
+CUDA_VISIBLE_DEVICES=$GPU python run.py \
     --model_name $MODEL \
     --task_name $TASK \
     --output_dir result/$TASK-${MODEL_NAME}-$TAG --tag $TAG --train_set_seed $SEED --num_train $TRAIN --num_dev $DEV --num_eval $EVAL --logging_steps 10 \
